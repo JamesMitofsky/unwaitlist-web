fa4f8beaffe2262275b07861ae49979580d51318
James <jamestedesco802@gmail.com>
2019-08-04 12:24:05 -0400

    Added contact and order confirmation pages

---
+++ pages/order-confirmation.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout justify-center align-center wrap>
+    <v-flex xs12 sm10 md8 xl7>
+
+      <div class="content-block">
+        <h2>Confirmed order</h2>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style>
+</style>
+
