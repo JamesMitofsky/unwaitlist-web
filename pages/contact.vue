a7416aac785944019e8ddcb314435f63d9e0ef4d
James <jamestedesco802@gmail.com>
2019-08-05 12:48:50 -0400

    Made set content width to match other pages in v-layout

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout justify-center align-center wrap>
-    <v-flex>
+    <v-flex xs12 sm10 md8 xl7>
       <div class="content-block">
         <h2>Contact</h2>
         <v-layout wrap>
