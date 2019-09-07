c9162eb5e691a6245efb727bd702e8d028052ba2
James <jamestedesco802@gmail.com>
2019-09-06 23:23:40 -0400

    Bare bones functioning Google Form with ability to custom style

---
+++ pages/form.vue
@@ -0,0 +1,23 @@
+<template>
+    <form class="form" action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdL6QgGVjqsDvZsEx__yltIvMmzWdfCX4NHksRmbgC1JQOrYA/formResponse">
+
+
+          <label>Email</label>
+          <input name="entry.1057850542" type="email" required />
+        
+          <label>First Name</label>
+          <input name="entry.175684321" type="text" required />
+
+          <label>Last Name</label>
+          <input name="entry.1110633439" type="text" required />
+
+          
+          <label>CRNs (Course Registration Numbers)</label>
+          <input name="entry.232057564" type="number" required />
+
+          
+
+          <input type="submit" value="Send" />
+
+    </form>
+</template>
