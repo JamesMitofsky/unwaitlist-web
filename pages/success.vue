2e09fed4aafd178c7eb61f7bb4d27bb3dcc1699b
James <jamestedesco802@gmail.com>
2019-09-07 13:08:40 -0400

    Updated copy on Success page and added Venmo links

--- pages/success.vue
+++ pages/success.vue
@@ -1,6 +1,30 @@
 <template>
-    <v-layout column>
-        <div>Success! Thanks for submitting your request.</div>
-        <div>Now, just venmo me the cost of your package and you'll be set.</div>
-    </v-layout>
+  <v-layout column>
+    <v-card>
+      <v-card-title>Request Submitted!</v-card-title>
+      <v-card-text
+        class="text--primary"
+      >An receipt of confirmation will be emailed soon. To finish up, just Venmo me the cost of the package you requested and I'll activate it on my end.</v-card-text>
+      <div class="payment-methods">
+        <a href="https://venmo.com/JamesTedesco">Finish by paying through Venmo.</a>
+
+        <a href="https://venmo.com/JamesTedesco">
+          <img class="venmo-code" src="../static/jamesVenmo.png" alt="Venmo QR Code" />
+        </a>
+      </div>
+    </v-card>
+  </v-layout>
 </template>
+
+<style scoped>
+.venmo-code {
+  max-width: 200px;
+}
+
+.payment-methods {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
