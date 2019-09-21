c84749152b5df88a3873e31f28e7691125101904
James <jamestedesco802@gmail.com>
2019-09-21 00:21:19 -0400

    Moved test amazon button to success page

--- pages/success.vue
+++ pages/success.vue
@@ -12,7 +12,27 @@
           <img class="venmo-code" src="../static/jamesVenmo.png" alt="Venmo QR Code" />
         </a>
       </div>
+      <script
+            async
+            src="https://static-na.payments-amazon.com/OffAmazonPayments/us/sandbox/js/Widgets.js"
+          ></script>
+          <div
+            data-ap-widget-type="expressPaymentButton"
+            data-ap-signature="LEDkAz7ZPProiEa%2B2MboTAzc1IR8Owf87BbaX6kC%2FM8%3D"
+            data-ap-seller-id="AI7X50X1F3K0H"
+            data-ap-access-key="AKIAI6SDNUAZDUSL6FQQ"
+            data-ap-lwa-client-id="amzn1.application-oa2-client.010359452bae48f4afce984eb29a3e32"
+            data-ap-return-url="https://unwaitlist.io/amazon-success"
+            data-ap-currency-code="USD"
+            data-ap-amount="5"
+            data-ap-note="testy test dis note, thanks"
+            data-ap-shipping-address-required="false"
+            data-ap-payment-action="AuthorizeAndCapture"
+            color="DarkGray"
+          ></div>
     </v-card>
+    
+          
   </v-layout>
 </template>
 
