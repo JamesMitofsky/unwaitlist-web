9183601e80598b51e6eab99c0f7709962c861241
James <jamesmitofsky@gmail.com>
2025-02-26 15:01:03 +0100

    Update copy

--- pages/success.vue
+++ pages/success.vue
@@ -6,9 +6,9 @@
         class="text--primary"
       >A confirmation receipt will be emailed soon. To finish up, just Venmo me the cost of the package you requested and I'll activate it on my end.</v-card-text>
       <div class="payment-methods">
-        <a href="https://venmo.com/JamesTedesco">Finish by paying through Venmo.</a>
+        <a href="https://venmo.com/JamesMitofsky">Finish by paying through Venmo.</a>
 
-        <a href="https://venmo.com/JamesTedesco">
+        <a href="https://venmo.com/JamesMitofsky">
           <img class="venmo-code" src="../static/jamesVenmo.png" alt="Venmo QR Code" />
         </a>
       </div>
